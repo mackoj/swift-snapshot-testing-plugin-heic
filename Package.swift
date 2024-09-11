699c81dc86c67d76efdcee2c058b7baaa159c8c6
Jeffrey Macko <github.jm@macko.fr>
2024-09-11 14:09:20 +0200

    feat: add documentation

--- Package.swift
+++ Package.swift
@@ -5,26 +5,27 @@ import PackageDescription
 
 let package = Package(
   name: "swift-snapshot-testing-plugin-heic",
+  
+  // TODO: update this with more tested platforms
   platforms: [
     .iOS(.v14),
     .macOS(.v11),
     .tvOS(.v14),
     .watchOS(.v7),
   ],
+  
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "HEICImageSerializer",
       targets: ["HEICImageSerializer"]
     ),
   ],
+  
   dependencies: [
-    //    .package(path: "/Users/jeffreymacko/Developer/swift-snapshot-testing"),
-    .package(url: "https://github.com/mackoj/swift-snapshot-testing.git", revision: "54c11f6"),
+    .package(url: "https://github.com/mackoj/swift-snapshot-testing.git", revision: "b899f13"),
   ],
+  
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "HEICImageSerializer",
       dependencies: [
--- Sources/HEICImageSerializer/HEICImageSerializer.swift
+++ Sources/HEICImageSerializer/HEICImageSerializer.swift
@@ -9,11 +9,30 @@ import UIKit
 import AppKit
 #endif
 
+/// A serializer class for handling HEIC image format encoding and decoding.
+///
+/// The `HEICImageSerializer` class conforms to the `ImageSerializationPlugin` protocol and provides functionality
+/// to encode and decode images in the HEIC (High Efficiency Image Coding) format. It integrates with the plugin-based
+/// image serialization architecture and uses the Core Graphics (`CGImageDestination`) APIs to perform the conversions.
 public class HEICImageSerializer: ImageSerializationPlugin {
+
+  /// Initializes a new instance of the `HEICImageSerializer`.
+  ///
+  /// The initializer is required to conform to the `ImageSerializationPlugin` protocol.
   required public init() {}
+  
+  /// The image format handled by this serializer, which is `heic`.
   static public let imageFormat: ImageSerializationFormat = .plugins("heic")
+  
+  /// A unique identifier for this serializer plugin.
   static public let identifier: String = "ImageSerializationPlugin.heic"
   
+  /// Encodes a `SnapImage` into HEIC format.
+  ///
+  /// This method converts the provided `SnapImage` into HEIC format using Core Graphics' `CGImageDestination`.
+  ///
+  /// - Parameter image: The image to be encoded.
+  /// - Returns: A `Data` object containing the encoded HEIC image, or `nil` if the encoding fails.
   public func encodeImage(_ image: SnapImage) -> Data? {
 #if !os(macOS)
     guard let cgImage = image.cgImage else { return nil }
@@ -28,6 +47,12 @@ public class HEICImageSerializer: ImageSerializationPlugin {
     return data as Data
   }
   
+  /// Decodes HEIC data into a `SnapImage`.
+  ///
+  /// This method converts the provided HEIC data into a `SnapImage`.
+  ///
+  /// - Parameter data: The HEIC data to be decoded.
+  /// - Returns: A `SnapImage` created from the data, or `nil` if the decoding fails.
   public func decodeImage(_ data: Data) -> SnapImage? {
 #if !os(macOS)
     return UIImage(data: data)
